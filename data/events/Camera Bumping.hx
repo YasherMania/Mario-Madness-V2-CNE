62c805cc02c023c7d896e536fd2087d6419efbe9
APurples <tylersanders0714@gmail.com>
2024-02-04 10:17:33 -0600

    changed paranoia events for the new cne update

--- data/events/Camera Bumping.hx
+++
@@ -1,6 +0,0 @@
-function onEvent(_){
-    if (_.event.name == "Camera Bumping"){
-        camZoomingInterval = _.event.params[0];
-        camZoomingStrength = _.event.params[1];
-    }
-}
--- data/events/Play Animation.hx
+++
@@ -1,6 +0,0 @@
-
-function onEvent(_) {
-	if (_.event.name == "Play Animation")
-		for (char in strumLines.members[_.event.params[0]].characters)
-            char.playAnim(_.event.params[1], _.event.params[2]);
-}
