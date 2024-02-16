5b4e968bc90867dc61b6c995025134d97d72ffa7
bromaster922 <118091335+bromaster922@users.noreply.github.com>
2024-02-15 20:35:12 -0700

    hey apurples
    ur kool!!!!!! ;3

--- data/events/Show Song.hx
+++ data/events/Show Song.hx
@@ -89,7 +89,7 @@ function postCreate() {
 function onEvent(_){
     if (_.event.name == "Show Song" && _.event.params[0]){
         autorText.text = _.event.params[1];
-        porterText.text = "Porter(s): " + _.event.params[2];
+        porterText.text = "Ported By: " + _.event.params[2];
         window.title = "Friday Night Funkin': Mario's Madness | " + PlayState.SONG.meta.displayName + " | " + autorText.text + " | " + porterText.text;
         for (i in [titleText, autorText, porterText/*, line1, line2*/]){
             if (downscroll) FlxTween.tween(i, {alpha: 1, y: i.y + 30}, 0.5, {ease: FlxEase.cubeOut});
