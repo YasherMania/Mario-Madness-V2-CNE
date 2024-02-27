a142375fcd52534e6bf00cee3c54acc19270794c
Yasher <119639489+YasherMania@users.noreply.github.com>
2024-02-26 21:38:52 -0600

    Botplay Strum change
    Still no note changes though still gotta figure that out lmfao

--- songs/Note.hx
+++ songs/Note.hx
@@ -1,13 +1,43 @@
-function onNoteCreation(event) {
-    event.noteSprite = 'game/notes/Mariodefault';
+function onNoteCreation(e) {
+    e.noteSprite = FlxG.save.data.botplayOption ? "game/notes/Luigidefault" : "game/notes/Mariodefault";
+}
+
+function onStrumCreation(e) {
+    e.sprite = FlxG.save.data.botplayOption ? "game/notes/Luigidefault" : "game/notes/Mariodefault";
+}
+
+function onSubstateClose() {
     if (FlxG.save.data.botplayOption) {
-        event.noteSprite = 'game/notes/Luigidefault';
+        changeStrumsTexture(player, "game/notes/Luigidefault");
+        changeStrumsTexture(cpu, "game/notes/Luigidefault");
+    } else {
+        changeStrumsTexture(player, "game/notes/Mariodefault");
+        changeStrumsTexture(cpu, "game/notes/Mariodefault");
     }
 }
 
-function onStrumCreation(event) {
-    event.sprite = 'game/notes/Mariodefault';
-    if (FlxG.save.data.botplayOption) {
-        event.sprite = 'game/notes/Luigidefault';
-    }
+function changeStrumsTexture(strumline:StrumLine, texture:String) {
+    for(strum in strumline.members) changeStrumTexture(strum, texture, strumline.strumAnimPrefix[strum.ID], strumline.strumScale);
+}
+var strumAnimPrefix = ["left", "down", "up", "right"];
+function changeStrumTexture(strum:Strum, texture:String, animPrefix:String, strumScale:Float) {
+    var oldAnimName:String = strum.animation.name;
+    var oldAnimFrame:Int = strum.animation?.curAnim?.curFrame;
+    strum.frames = Paths.getFrames(texture);
+
+    strum.antialiasing = true;
+    strum.scale.set(1, 1);
+    strum.updateHitbox();
+    strum.setGraphicSize(Std.int((strum.width * 0.7) * strumScale));
+
+    strum.animation.addByPrefix('static', 'arrow' + animPrefix.toUpperCase());
+    strum.animation.addByPrefix('pressed', animPrefix+' press', 24, false);
+    strum.animation.addByPrefix('confirm', animPrefix+' confirm', 24, false);
+
+    strum.animation.curAnim = null;
+
+    strum.updateHitbox();
+
+    strum.playAnim(oldAnimName, true);
+    strum.animation?.curAnim?.curFrame = oldAnimFrame;
 }
